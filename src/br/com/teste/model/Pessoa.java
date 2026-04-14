707a2ee5c55985e1443e277e5caa104916eea0ae
FranzenDev <isaacfranzen@outlook.com>
2026-04-14 09:02:12 -0300

    Subindo as classes "Pessoa" e "Funcionario"

---
+++ src/br/com/teste/model/Pessoa.java
@@ -0,0 +1,13 @@
+package br.com.teste.model;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    String nome;
+    LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+}
